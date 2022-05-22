e8f24fff29ee6f2e2431cd7f26d975a7dd0c1d97
Dan <danilobomfim.si@gmail.com>
2022-05-22 19:05:38 -0300

    Fix

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function App(props) {
+  return (
+    <>
+      <input type="text" name="usuario" id="usuario" className="userInput" />
+      <button id="pesquisar">Pesquisar</button>
+    </>
+  );
+}
+
+export default App;
